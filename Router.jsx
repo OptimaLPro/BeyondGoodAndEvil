e42571e4b2c890f83a2c1b6be9f47b5b0c235b7c
Nati Forish <forish.nati@gmail.com>
2024-07-17 17:36:45 +0300

    first commit

---
+++ Router.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./src/components/Home/Home";
+
+const Router = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
+    );
+}
+
+export default Router;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <>
+            <div className="bg-[#f5f3f1] h-screen">
+                asd
+            </div>
+        </>
+    );
+}
+
+export default Home;
