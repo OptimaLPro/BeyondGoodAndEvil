afc2ea4a70c894216f84b092e0fb397016ff85a9
Nati Forish <optimalpro11@gmail.com>
2024-08-26 17:32:40 +0300

    Change Github hooks

--- src/components/hooks/ScrollToTop.jsx
+++ src/components/hooks/ScrollToTop.jsx
@@ -6,7 +6,7 @@ const ScrollToTop = () => {
 
     useEffect(() => {
         window.scrollTo(0, 0);
-        console.log('scrolling to top!');
+        console.log('scrolling to top');
     }, [pathname]);
 
     return null;
